c053d9cf1b2f1c9c6f1b4bbc3721a6dc5ee58baa
Sarah Beaver <sbeaver57@yahoo.com>
2021-02-10 15:52:15 -0600

    Create guess_key.py

---
+++ guess_key.py
@@ -0,0 +1,22 @@
+from utils_demo import *
+
+# for i in range(1,4):
+message=read_file("m"+str(1)+".txt")
+cipher=read_bytes("c"+str(1)+".bin")
+nonce=read_bytes("nonce"+str(1)+".bin")
+key=bin(2 ** 127)
+foundkey=False
+count =0
+# print(bitstring_to_bytes(key))
+while foundkey==False:
+    # print(decryptor_CTR(cipher,nonce,key))
+    # print(message)
+    print(hex(int(key,2)))
+    if decryptor_CTR(cipher,nonce,bitstring_to_bytes(key)) == string_to_bytes(message):
+        print(decryptor_CTR(cipher,nonce,bitstring_to_bytes(key)))
+        print(message)
+        foundkey =True
+        print(key)
+    else:
+        key=bin(int(key,2)+1)
+
