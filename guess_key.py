c44b067f3f4dc4a834210d5f35424736563799f1
Sarah Beaver <sbeaver57@yahoo.com>
2021-02-10 19:43:32 -0600

    Update guess_key.py

--- guess_key.py
+++ guess_key.py
@@ -1,9 +1,9 @@
 from utils_demo import *
 
 for i in range(1,4):
-    message=read_file("m"+str(1)+".txt")
-    cipher=read_bytes("c"+str(1)+".bin")
-    nonce=read_bytes("nonce"+str(1)+".bin")
+    message=read_file("m"+str(i)+".txt")
+    cipher=read_bytes("c"+str(i)+".bin")
+    nonce=read_bytes("nonce"+str(i)+".bin")
     key=bin(2 ** 127)
     foundkey=False
     count =0
@@ -13,8 +13,8 @@ for i in range(1,4):
         # print(message)
         # print(hex(int(key,2)))
         if decryptor_CTR(cipher,nonce,bitstring_to_bytes(key)) == string_to_bytes(message):
-            print(decryptor_CTR(cipher,nonce,bitstring_to_bytes(key)))
-            print(message)
+            # print(decryptor_CTR(cipher,nonce,bitstring_to_bytes(key)))
+            # print(message)
             foundkey =True
             print(hex(int(key,2)))
         else:
